28f4a9658d7dfb2d8e2b38764161d45c47f522bc
K0Fran <110508436+K0Fran@users.noreply.github.com>
2024-04-27 22:55:38 -0400

    Add files via upload

---
+++ juego.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+#include "dinosprites.h"
+
+int main() {
+    char endGame = 'n';
+
+    while (endGame != 'y') {
+        system("clear"); 
+        dino1();
+        std::this_thread::sleep_for(std::chrono::milliseconds(200)); 
+
+        system("clear");
+        dino2();
+        std::this_thread::sleep_for(std::chrono::milliseconds(200)); 
+
+        system("clear");
+        dino3();
+        std::this_thread::sleep_for(std::chrono::milliseconds(200)); 
+
+        std::cout << "Y= fin del juego: ";
+        std::cin >> endGame;
+    }
+
+    std::cout << "Game Over!" << std::endl;
+
+    return 0;
+}
